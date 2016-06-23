7e3a3788625f2b6dfb5f84c8f94259f89cd5ff19
pickledyamsman <pickledyamsman@gmail.com>
2016-06-23 10:03:27 -0400

    serialized data to work

--- app/assets/javascripts/events/eventsCtrl.js
+++ app/assets/javascripts/events/eventsCtrl.js
@@ -1,5 +1,5 @@
 function EventsCtrl($scope, eventsService, event){
-  $scope.event = event;
+  $scope.event = sanitizeEvents(event);
   $scope.food_types = [{id: '1'}];
   
   $scope.addNewType = function(){
@@ -19,7 +19,7 @@ function EventsCtrl($scope, eventsService, event){
       avg_price: $scope.avg_price,
       location: $scope.location,
     }).success(function(restaurant){
-      $scope.event.restaurants.push(restaurant);
+      $scope.event.restaurants.push(sanitizeRestaurant(restaurant));
     });
 
     $scope.name = '';
@@ -39,9 +39,23 @@ function EventsCtrl($scope, eventsService, event){
     eventsService.downvoteRestaurant(event, restaurant);
   };
 
+  function sanitizeEvents(event) {
+     event['restaurants'] = event['restaurants'].map(function (res) {
+         return sanitizeRestaurant(res);
+     });
+     return event;
+  }
 
+  function sanitizeRestaurant(res) {
+     res['food_type'] = JSON.parse(res['food_type'].replace(/=>/gi, ":")); // Replace => with  : so it will deserialize
+     res['food_type'] = res['food_type'].map(function (ft) {
+         ft['id'] = ft.id | 0; // Binary cast to int
+        return ft;
+     });
+     return res;
+  }
 }
 
 angular
   .module('app')
-  .controller('EventsCtrl', EventsCtrl)
+  .controller('EventsCtrl', EventsCtrl);
